b293b3194f6a2bde9ead3764e3eee3a5ff5678f1
Mar 🌊 <mariajara929@gmail.com>
2024-09-12 14:01:22 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,35 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  // condition ? doThisIfTrue : doThisIfFalse;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Name" : variables.name} ${
+    variables.lastNamename == null ? "Last Name" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "Role" : variables.role}</h2>
+          <h3>${variables.city == null ? "City" : variables.city}, ${
+    variables.country == null ? "Country" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a target=”_blank” href="https://twitter.com/${
+              variables.twitter == null ? "4geeksacademy" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a target=”_blank” href="https://github.com/${
+              variables.github == null ? "4geeksacademy" : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a target=”_blank” href="https://linkedin.com/${
+              variables.linkedin == null
+                ? "school/4geeksacademy"
+                : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a target=”_blank” href="https://instagram.com/${
+              variables.instagram == null
+                ? "4geeksacademy"
+                : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -49,6 +65,7 @@ function render(variables = {}) {
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
+
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
